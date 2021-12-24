a19132cdb3d700a1a32996ce50aa5283c9f5b0b8
Lothering0 <danil2005232@gmail.com>
2021-12-24 17:42:31 +0300

    Implemented animal class

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 "use strict";
-class Animal {
-    constructor({ name, age, hasTail }) {
-        this.name = name;
-        this.age = age;
-        this.hasTail = hasTail;
+var Animal;
+(function (Animal_1) {
+    class Animal {
+        constructor({ name, age, hasTail }) {
+            this.name = name;
+            this.age = age;
+            this.hasTail = hasTail;
+        }
+        voice() {
+            console.log(this.name);
+        }
     }
-}
+    Animal.type = 'animal'.toUpperCase();
+    class Cat extends Animal {
+        constructor({ color, ...config }) {
+            super(config);
+            this.color = color;
+        }
+        get ageInfo() {
+            return this.age * 7;
+        }
+    }
+    Cat.type = 'cat'.toUpperCase();
+    Animal_1.Cat = Cat;
+})(Animal || (Animal = {}));
+const cat = new Animal.Cat({
+    name: 'Cat',
+    age: 5,
+    hasTail: true,
+    color: 'black'
+});
+console.log(cat.ageInfo);
--- index.ts
+++ index.ts
@@ -1,17 +1,60 @@
-interface Options {
-  name: string
-  age: number
-  hasTail: boolean
-}
+namespace Animal {
+  export interface Options {
+    name: string
+    age: number
+    hasTail: boolean
+  }
 
-class Animal implements Options {
-  public name: string
-  public age: number
-  public hasTail: boolean
+  export interface IAnimal extends Options {
+    voice: () => void
+  }
 
-  constructor({ name, age, hasTail }: Options) {
-    this.name = name
-    this.age = age
-    this.hasTail = hasTail
+  export interface ICat extends Options {
+    color: string
+    ageInfo?: number
   }
 }
+
+namespace Animal {
+  abstract class Animal implements IAnimal {
+    static type = 'animal'.toUpperCase()
+
+    name: string
+    age: number
+    hasTail: boolean
+
+    constructor({ name, age, hasTail }: Options) {
+      this.name = name
+      this.age = age
+      this.hasTail = hasTail
+    }
+
+    voice(): void {
+      console.log(this.name)
+    }
+  }
+
+  export class Cat extends Animal implements ICat {
+    static type = 'cat'.toUpperCase()
+
+    color: string
+
+    constructor({ color, ...config }: ICat) {
+      super(config)
+      this.color = color
+    }
+
+    get ageInfo() {
+      return this.age * 7
+    }
+  }
+}
+
+const cat: Animal.ICat = new Animal.Cat({
+  name: 'Cat',
+  age: 5,
+  hasTail: true,
+  color: 'black'
+})
+
+console.log(cat.ageInfo)
