397c3fe70b1e278f644feda81a29d2aa209574c4
Lothering0 <danil2005232@gmail.com>
2021-12-25 14:23:45 +0300

    Object Proxy

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-"use strict";
-const delay = (ms) => new Promise(resolve => setTimeout(() => resolve(null), ms));
-const url = 'https://jsonplaceholder.typicode.com/todos';
-async function fetchAsyncTodos() {
-    try {
-        await delay(2000);
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(data);
-    }
-    catch (e) {
-        console.error(e.message);
-    }
-    finally {
-        console.log('Done');
-    }
+const person = {
+  name: 'John',
+  age: 25,
+  job: 'Fullstack'
 }
-fetchAsyncTodos();
+
+const op = new Proxy(person, {
+  get(target, prop) {
+    console.log(target)
+    console.log(prop)
+    return target[prop]
+  },
+  set(target, prop, value) {
+    if (prop in target) {
+      target[prop] = value
+    } else {
+      throw new Error(`No ${prop} field in target`)
+    }
+  },
+  has(target, prop) {
+    return ['age', ',,,', 'name', 'job'].includes(prop)
+  },
+  deleteProperty(target, prop) {
+    console.log('Deleting', prop)
+    delete target[prop]
+  }
+})
+
+delete op.age
+// op.age = 123
+console.log(op.age)
--- index.ts
+++ index.ts
@@ -1,27 +1,19 @@
-const delay = (ms: number) => new Promise(resolve => setTimeout(() => resolve(null), ms))
-
-interface Todo {
-  completed: boolean
-  id: number
-  title: string
-  userId: number
+/* interface Human {
+  name: string
+  age: number
+  job: string
 }
 
-const url: string = 'https://jsonplaceholder.typicode.com/todos'
+// Objects
+const person: Human = {
+  name: 'John',
+  age: 25,
+  job: 'Fullstack'
+}
 
-async function fetchAsyncTodos() {
-  try {
-    await delay(2000)
-
-    const response = await fetch(url)
-    const data: Todo[] = await response.json()
-
-    console.log(data)
-  } catch (e) {
-    console.error((e as Error).message)
-  } finally {
-    console.log('Done')
+const op = new Proxy(person, {
+  get(target, prop): any {
+    return target[prop]
   }
-}
-
-fetchAsyncTodos()
+})
+ */
