e96bb729d6e32c0761b78d375c4aef5fa0bd05cf
Lothering0 <danil2005232@gmail.com>
2021-12-26 21:54:12 +0300

    Non-binary tree, total value

--- index.js
+++ index.js
@@ -1,7 +1,80 @@
-"use strict";
-const first = () => console.log('first');
-const second = () => console.log('second');
-const third = () => console.log('third');
-first();
-setTimeout(second, 0);
-third();
+const tree = [
+  {
+    v: 5,
+    c: [
+      {
+        v: 10,
+        c: [
+          {
+            v: 11
+          }
+        ]
+      },
+      {
+        v: 7,
+        c: [
+          {
+            v: 5,
+            c: [
+              {
+                v: 1
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    v: 5,
+    c: [
+      {
+        v: 10
+      },
+      {
+        v: 15
+      }
+    ]
+  }
+]// 69
+
+const recursive = tree => {
+  let total = 0
+
+  tree.forEach(el => {
+    total += el.v
+
+    if (!el.c) return total
+
+    total += recursive(el.c)
+  })
+
+  return total
+}
+
+// console.log(recursive(tree))
+
+const iteration = tree => {
+  if (!tree.length) return 0
+
+  const stack = []
+  let total = 0
+
+  tree.forEach(node => {
+    stack.push(node)
+  })
+
+  while (stack.length) {
+    const node = stack.pop()
+    total += node.v
+    if (node.c) {
+      node.c.forEach(el => {
+        stack.push(el)
+      })
+    }
+  }
+
+  return total
+}
+
+console.log(iteration(tree))
